eaf3803c88e5678fb645e0cf5efb8025f7a0b4c3
FranSollima <francisco@sollima.net>
2024-01-15 19:50:01 -0300

    Agregadas funciones a nlp base

--- NLP/nlp_base.py
+++ NLP/nlp_base.py
@@ -16,6 +16,24 @@ class NLPBase:
     def extract_adjectives(self, doc):
         raise NotImplementedError("Subclasses must implement the extract_adjectives method.")
 
+    def count_entity_types(self, doc):
+        raise NotImplementedError("Subclasses must implement the count_entity_types method.")
+    
+    def extract_entity_sentiments(self, doc):
+        raise NotImplementedError("Subclasses must implement the extract_entity_sentiments method.")
+    
+    def extract_place(self, doc):
+        raise NotImplementedError("Subclasses must implement the extract_place method.")
+    
+    def extract_date(self, doc):
+        raise NotImplementedError("Subclasses must implement the extract_date method.")
+    
+    def extract_sources(self, doc):
+        raise NotImplementedError("Subclasses must implement the extract_sources method.")
+    
+    def extract_links(self, doc):
+        raise NotImplementedError("Subclasses must implement the extract_links method.")
+    
     def count_adjectives(self, doc):
         return len(self.extract_adjectives(doc))
 
