beb2ee0f8fa28111b9ce2b6a9e1e2cf42073a3af
JuanCruzC97 <juancruzcamacho97@gmail.com>
2024-04-23 20:35:40 -0300

    minor changes in label studio setup

--- label_studio/label_studio_setup.py
+++ label_studio/label_studio_setup.py
@@ -10,41 +10,88 @@ from trustmonitor import import_utils
 from trustmonitor.articles import ArticlesCorpus
 
 
+def post_scrapp_processing(news_list):
+    
+    rules = [('Redacción LAVOZ','\n')]
+ 
+    for news in news_list:
+        for k in news:
+            if isinstance(news[k], str):
+                for rule in rules:
+                    news[k] = news[k].replace(rule[0],rule[1])
+                    
+    return news_list
+
+def post_scrapp_processing_fecha(news_list):
+
+    month_mapping = {"enero":1,
+                     "febrero":2,
+                     "marzo":3,
+                     "abril":4,
+                     "mayo":5,
+                     "junio":6,
+                     "julio":7,
+                     "agosto":8,
+                     "septiembre":9,
+                     "octubre":10,
+                     "noviembre":11,
+                     "diciembre":12}
+
+    for news in news_list:
+        
+        fecha, fecha_hora = news["fecha_hora"].split(",")
+        fecha = fecha.split(" de ")
+        fecha = f"{fecha[0]}/{month_mapping[fecha[1]]}/{fecha[2]}"
+        
+        news["fecha_hora"] = fecha_hora
+        news["fecha"] = fecha
+        
+    return news_list
+
+def post_scrapp_processing_medio(news_list):
+    
+    for news in news_list:
+        news['medio'] = 'lavoz'
+
+    return news_list
+
+def post_scrapp_processing_paragraph(news_list):
+ 
+    for news in news_list:
+        
+        text_list = list(news["cuerpo"])
+                    
+        for i in range(len(text_list)-1):
+            if text_list[i] == "." and text_list[i+1] != " " and not (text_list[i-1].isdigit() and text_list[i+1].isdigit()) and text_list[i+1].isupper() :
+                text_list[i] = ".\n"
+                
+        news["cuerpo"] = ''. join(text_list)
+                    
+    return news_list
+
 ROOT = import_utils.get_project_root()
 noticias = import_utils.import_news_from_json(f'{ROOT}/scraper/data_noticias_lavoz.json')
+noticias = post_scrapp_processing(noticias)
+noticias = post_scrapp_processing_fecha(noticias)
+noticias = post_scrapp_processing_medio(noticias)
+noticias = post_scrapp_processing_paragraph(noticias)
 
-month_mapping = {"enero":1,
- "febrero":2,
- "marzo":3,
- "abril":4,
- "mayo":5,
- "junio":6,
- "julio":7,
- "agosto":8,
- "septiembre":9,
- "octubre":10,
- "noviembre":11,
- "diciembre":12}
-
-for news in noticias:
-    news['medio'] = 'lavoz'
-    
-    fecha, fecha_hora = news["fecha_hora"].split(",")
-    fecha = fecha.split(" de ")
-    fecha = f"{fecha[0]}/{month_mapping[fecha[1]]}/{fecha[2]}"
-    
-    news["fecha_hora"] = fecha_hora
-    news["fecha"] = fecha
-    
 filename = f'{ROOT}/label_studio/data/raw/data_noticias_lavoz_formatted.json'
    
 with open(filename, 'w', encoding='utf-8') as f:
     json.dump(noticias, f, ensure_ascii=False, indent=4)
     
-    
+
 # Load news from json file. 
 corpus = ArticlesCorpus()
 corpus.load_articles(noticias)
+
+# Filter por categorias negocios y política.
+df = corpus.get_catalog().query("seccion.str.contains('Política') | seccion.str.contains('Negocios')")
+
+corpus2 = corpus.filter_by_catalog(df, to_corpus=True)
+corpus2.reset_index()
+corpus2.export_articles(f"{ROOT}/label_studio/data/raw/data_noticias_lavoz_politica_negocios.json")
 
 n_blocks = 10
 n_news_per_block = 20
@@ -54,5 +101,5 @@ for i in range(n_blocks):
     e = i*n_news_per_block + n_news_per_block
     index_range = np.arange(s, e)
     
-    corpus_to_export = corpus.filter_by_index(index_range, to_corpus=True)
-    corpus_to_export.export_articles(f'{ROOT}/label_studio/data/inputs/data_noticias_lavoz_{s}_{e}.json')
+    corpus_to_export = corpus2.filter_by_index(index_range, to_corpus=True)
+    corpus_to_export.export_articles(f'{ROOT}/label_studio/data/inputs/data_noticias_lavoz_{s}_{e-1}.json')
